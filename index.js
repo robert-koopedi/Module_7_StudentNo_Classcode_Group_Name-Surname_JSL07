0185ea9bf808abdb10fd7d16af62757a77e663fb
robert koopedi <koopedi05@gmail.com>
2025-01-15 10:09:11 +0200

    Added eventlistener to close button

--- index.js
+++ index.js
@@ -25,12 +25,18 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
-      <h3>${studentName}</h3>
+      certificateContent.innerHTML = `
+      <h2>Certificate of Achievement</h2>
+      <p>This is to certify that</p>
+      <h1>${studentName}</h1>
+      <p>has almost completed the </p>
+      <h4> ${courseName}</h4>
+      <P>${personalMessage}</p>
     `;
     
       //  Display the modal
       modal.style.display = 'block';
+      
   
       // Clear the form inputs
       studentNameInput.value = '';
@@ -39,8 +45,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
+    closeModal.addEventListener('click', function () {
+          modal.style.display = 'none';
+        });
       
     });
-  });
+  ;
   
+
+
+ 
